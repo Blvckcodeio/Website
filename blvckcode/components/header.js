2869a084326275f439b1893215249244a0932c1e
Kayode Soliu <admin@blvckcode.io>
2023-09-21 16:06:52 +0100

    Commit

--- blvckcode/components/header.js
+++ blvckcode/components/header.js
@@ -1,15 +1,18 @@
 import Image from 'next/image'
-import ball from "./svgs/balls.svg"
+import ball from "./svgs/ball.png"
 
 function Header() {
     return(
             <div className="hContainer">
                 <div className="htContainer">
                     <text className="htext">Experience smart living</text>
+                    <div className="bel-text">
                     <div className="turn-on-btn">
                         <div className="turn-ball">
-                            <Image src={ball} alt="desc"/>
+                            <Image src={ball} height= "20" width="20" alt="desc"/>
                         </div>
+                        <p className='btn-text'>Turn Off</p>
+                    </div>
                     </div>
                 </div>
             </div>
