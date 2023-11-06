9ff2778a6d49bd7a078ccc2f906268fea77098ed
Theprincejor <ninokayglobal@gmail.com>
2023-11-06 15:48:41 +0100

    Commit

--- blvckcode/components/navbar.js
+++ blvckcode/components/navbar.js
@@ -1,19 +1,49 @@
+import { useState, useEffect } from 'react';
 
 function Navbar({navbar, navButton}) {
   
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        // Update the state only once after mount
+        const handleResize = () => {
+          if (window.innerWidth < 768) {
+            setIsMobile(true);
+          } else {
+            setIsMobile(false);
+          }
+        };
+    
+        // Initial check
+        handleResize();
+    
+        // Attach event listener
+        window.addEventListener('resize', handleResize);
+    
+        // Cleanup
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+    
     return(
         <>
             <div className={navbar}>
                      <div className="bName">
                        Blvckcode.io 
                      </div>
-                     <div className="navCont_flex">
+                     {isMobile &&
+                        <>
+                         <div className="navCont_flex">
                      <div className="navPages">Features</div>
                      <div className="navPages">Packages</div>
                      <div className="navPages">About</div>
                      <div className="navPages">Partners</div>
                      </div>
                      <div><button className={navButton}>Get Started</button></div>
+                     </>
+                     }
+                    
             </div>
         </>
     )
